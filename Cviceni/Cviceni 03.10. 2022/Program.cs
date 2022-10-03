28f0c081709b9fc63811bb5967ed9aed5783bca9
Ondřej Kulhavý <okulhav@gmail.com>
2022-10-03 08:15:32 +0200

    feat: to string and random list generation methods

--- Cviceni/Cviceni 03.10. 2022/Program.cs
+++ Cviceni/Cviceni 03.10. 2022/Program.cs
@@ -8,5 +8,23 @@ namespace Cviceni_03._10._2022
         {
             Console.WriteLine("Hello World!");
         }
+        
+    public static int[] GenerujPole(int dolni, int horni)
+    {
+        int[] pole = new int[10];
+        Random rnd = new Random();
+        for (int i = 0; i < pole.Length; i++)
+        {
+            pole[i] = rnd.Next(dolni, horni++);
+        }
+        return pole;
+    }
+    public static void Vypis(int[] pole)
+    {
+        for (int i = 0; i < pole.Length; i++)
+        {
+            Console.WriteLine(pole[i]);
+        }
+    }
     }
 }
