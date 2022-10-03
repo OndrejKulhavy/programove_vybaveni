b3ed2baeb16e6ab233f1f3d236608e6a480ba2d7
Ondřej Kulhavý <okulhav@gmail.com>
2022-10-03 07:40:32 +0200

    update: change in directories

---
+++ Cviceni/Cviceni 03.10. 2022/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cviceni_03._10._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
