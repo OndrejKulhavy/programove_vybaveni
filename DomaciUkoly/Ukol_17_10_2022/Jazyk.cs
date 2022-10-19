cd7ce3475551e74c1b957c56e02d181a8882209a
Ondřej Kulhavý <okulhav@gmail.com>
2022-10-19 16:55:30 +0200

    update: pridan header s autorstvim

--- DomaciUkoly/Ukol_17_10_2022/Jazyk.cs
+++ DomaciUkoly/Ukol_17_10_2022/Jazyk.cs
@@ -1,3 +1,9 @@
+/*
+ * Created Date: Wednesday, October 19th 2022, 12:17:27 pm
+ * Author: Ondřej Kulhavý
+ * 
+ * Copyright (c) 2022
+ */
 using System;
 using System.Collections.Generic;
 
--- DomaciUkoly/Ukol_17_10_2022/Program.cs
+++ DomaciUkoly/Ukol_17_10_2022/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Created Date: Wednesday, October 19th 2022, 12:17:27 pm
+ * Author: Ondřej Kulhavý
+ * 
+ * Copyright (c) 2022
+ */
+
+using System;
 using System.Collections.Generic;
 
 namespace Ukol_17_10_2022
